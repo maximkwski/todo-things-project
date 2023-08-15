2a94d153d91914a5088e73131117912fd9597b91
Maksym <makkie.makowski@gmail.com>
2023-08-15 14:41:51 -0400

    add project item form, add styles

--- src/components/projectItem.js
+++ src/components/projectItem.js
@@ -1,5 +1,6 @@
 import { projects } from "./todos";
 import form from "./form";
+import '../styles/projectItem.css';
 
 export function projectItem(proj) {
 
@@ -17,8 +18,104 @@ export function projectItem(proj) {
     projectDiv.appendChild(project_h2);
     projectDiv.appendChild(project_category);
     projectDiv.appendChild(project_due);
-   contentDiv.appendChild(projectDiv);
-   form();
+    contentDiv.appendChild(projectDiv);
+
+
+
+
+   //FORM
+   const createToDoElement = document.createElement('section');
+   createToDoElement.classList.add('create-todo');
+   const createToDoElement_h3 = document.createElement('h3');
+   createToDoElement_h3.textContent = 'Add Task';
+   createToDoElement.appendChild(createToDoElement_h3);
+   const todoForm = document.createElement('form');
+   todoForm.id = 'new-todo-form';
+   const todoForm_h4 = document.createElement('h4');
+   todoForm_h4.textContent = "What's on your project list?";
+   const todoForm_input = document.createElement('input');
+   todoForm_input.type = 'text';
+   todoForm_input.name = 'content';
+   todoForm_input.id = 'content';
+   todoForm_input.placeholder = "e.g make a video";
+   todoForm.appendChild(todoForm_h4);
+   todoForm.appendChild(todoForm_input);
+
+   const priorityDiv = document.createElement('div');
+   priorityDiv.classList.add('priority');
+   
+   const label1 = document.createElement('label'); // priority low
+   const label1_input = document.createElement('input');
+    label1_input.type = 'radio';
+    label1_input.name = 'priority';
+    label1_input.id = 'priority1';
+    label1_input.value = 'low';
+    const low_span = document.createElement('span');
+    low_span.classList.add('bbl');
+    low_span.classList.add('low');
+    const low_div = document.createElement('div');
+    low_div.textContent = 'Low';
+    label1.appendChild(label1_input);
+    label1.appendChild(low_span);
+    label1.appendChild(low_div);
+    priorityDiv.appendChild(label1);
+
+    const label2 = document.createElement('label'); // priority medium
+   const label2_input = document.createElement('input');
+    label2_input.type = 'radio';
+    label2_input.name = 'priority';
+    label2_input.id = 'priority2';
+    label2_input.value = 'medium';
+    const medium_span = document.createElement('span');
+    medium_span.classList.add('bbl');
+    medium_span.classList.add('medium');
+    const medium_div = document.createElement('div');
+    medium_div.textContent = 'Medium';
+    label2.appendChild(label2_input);
+    label2.appendChild(medium_span);
+    label2.appendChild(medium_div);
+    priorityDiv.appendChild(label2);
+
+    const label3 = document.createElement('label'); // priority high
+   const label3_input = document.createElement('input');
+    label3_input.type = 'radio';
+    label3_input.name = 'priority';
+    label3_input.id = 'priority3';
+    label3_input.value = 'high';
+    const high_span = document.createElement('span');
+    high_span.classList.add('bbl');
+    high_span.classList.add('high');
+    const high_div = document.createElement('div');
+    high_div.textContent = 'High';
+    label3.appendChild(label3_input);
+    label3.appendChild(high_span);
+    label3.appendChild(high_div);
+    priorityDiv.appendChild(label3);
+    todoForm.appendChild(priorityDiv);
+
+   const submit_btn = document.createElement('input');
+    submit_btn.type = 'submit';
+    submit_btn.value = 'Add To Do';
+    todoForm.appendChild(submit_btn); // add submit btn
+    
+    createToDoElement.appendChild(todoForm);
+    contentDiv.appendChild(createToDoElement);
+
+    todoForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        // const todo = {
+        //     content: e.target.elements.content.value,
+        //     category: e.target.elements.category.value,
+        //     done: false,
+        //     createdAt: new Date().getTime()
+
+        // }
+
+        // addTodo(todo);
+
+        e.target.reset();
+    })
 
     return contentDiv;
 }
