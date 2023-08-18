48b906333e35fb8d5391e5dc1bd07e190bdfc23f
Maksym <makkie.makowski@gmail.com>
2023-08-18 18:10:15 -0400

    add delete note btn, add styles to notes div

--- src/components/quickNotes.js
+++ src/components/quickNotes.js
@@ -1,12 +1,11 @@
 import '../styles/notes.css';
-import { notes, addNote, removeNote } from "./todos";
+import { notes, addNote, removeNote, saveNotes } from "./todos";
 
 export default function quickNotes() {
     const contentDiv = document.querySelector('#content');
 
     const notesElement = document.createElement('section');
     notesElement.id = 'notes';
-    notesElement.innerHTML = 'Quick notes';
 
     notes.forEach(note => {
         const noteItem = document.createElement('div'); //note item
@@ -15,13 +14,30 @@ export default function quickNotes() {
         const date_h3 = document.createElement('h3');
         date_h3.innerHTML = note.date;
 
-        const note_input = document.createElement('input');
-        note_input.type = 'text';
+        const note_input = document.createElement('textarea');
+        note_input.name = 'textarea';
+        note_input.value = `${note.content}`;
         note_input.placeholder = 'type your note here..';
+
+        const deleteBtn = document.createElement('div');
+        deleteBtn.classList.add('delete-note');
+        const span = document.createElement('span');
+        span.classList.add('cross');
+        deleteBtn.appendChild(span);
 
 
         noteItem.appendChild(date_h3);
+        noteItem.appendChild(note_input);
+        noteItem.appendChild(deleteBtn);
         notesElement.appendChild(noteItem);
+
+
+        note_input.addEventListener('blur', e => {
+            note.content = e.target.value;
+            saveNotes();
+            quickNotes();
+
+        })
     });
 
     const addNoteBtn = document.createElement('input');
@@ -38,6 +54,7 @@ export default function quickNotes() {
         addNote(note);
         console.log(notes);
     })
+
 
     contentDiv.appendChild(notesElement);
     
