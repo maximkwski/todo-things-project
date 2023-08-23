1c995b469632ebd456a716b34420ae864dbaa271
Maksym <makkie.makowski@gmail.com>
2023-08-23 15:53:02 -0400

    add burger menu, and media query for mobile screen size

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,32 @@ import headerPng from '../assets/logo-header.png';
 export default function header() {
     const headerElement = document.getElementById('header');
 
+    const burgerMenu = document.createElement('div');
+    burgerMenu.classList.add('burger-menu');
+    burgerMenu.id = 'burger';
+    for (let i = 0; i < 3; i++) {
+        const bar = document.createElement('span');
+        bar.classList.add('bar');
+        burgerMenu.appendChild(bar);
+    }
+
     const headerIcon = new Image();
     headerIcon.src = headerPng;
     const header_h1 = document.createElement('h1');
     header_h1.textContent = "To-Do-Things";
-    headerElement.appendChild(headerIcon);
-    headerElement.appendChild(header_h1);
+    const headerLogo = document.createElement('div');
+    headerLogo.classList.add('header-logo');
+    headerLogo.appendChild(headerIcon);
+    headerLogo.appendChild(header_h1);
+
+    headerElement.appendChild(burgerMenu);
+    headerElement.appendChild(headerLogo);
+
+    burgerMenu.addEventListener("click", () => {
+        const sideBar = document.querySelector('aside');
+        sideBar.classList.toggle("active");
+        burgerMenu.classList.toggle("burg-active")
+    });
 
     
 
